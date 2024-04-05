f92b358b5cadb0059d128e537acdfcc4bb220b5b
sonodima <sonodimaa@icloud.com>
2024-04-05 12:51:31 +0200

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,144 @@
+﻿using System.Linq;
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+using Steamworks;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+namespace BetterLobby;
+
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public sealed class Plugin : BaseUnityPlugin
+{
+    public static ManualLogSource CurLogger { get; private set; } = null;
+
+    private void Awake()
+    {
+        CurLogger = Logger;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        EscapeMenuUtils.CreateButton("FILL", "FILL LOBBY", () => LobbyUtils.MakePublic());
+    }
+}
+
+internal sealed class EscapeMenuUtils : MonoBehaviour
+{
+    internal static bool CreateButton(string name, string text, UnityAction action)
+    {
+        var uiButtonList = FindButtonList();
+        if (uiButtonList == null)
+        {
+            Plugin.CurLogger?.LogError("Failed to find the EscapeMenu control list!");
+            return false;
+        }
+
+        GameObject uiBaseControl = uiButtonList.transform.Find("RESUME")?.gameObject;
+        if (uiBaseControl == null)
+        {
+            Plugin.CurLogger?.LogError("Failed to find the 'RESUME' control!");
+            return false;
+        }
+
+        GameObject uiControl = Instantiate(uiBaseControl, uiButtonList.transform);
+        uiControl.name = name;
+
+        var uiButton = uiControl.GetComponentInChildren<Button>();
+        if (uiButton == null)
+        {
+            Plugin.CurLogger?.LogError("Failed to find the Button component!");
+            Destroy(uiControl);
+            return false;
+        }
+
+        var uiText = uiControl.GetComponentInChildren<TextMeshProUGUI>();
+        if (uiText == null)
+        {
+            Plugin.CurLogger?.LogError("Failed to find the TextMeshProUGUI component!");
+            Destroy(uiControl);
+            return false;
+        }
+
+        uiButton.onClick.AddListener(action);
+        // SynchronizationContext.Current.Post(_ => uiText.SetText(text), null);
+        uiText.SetCharArray(text.ToCharArray());
+        uiText.SetAllDirty();
+        return true;
+    }
+
+    private static GameObject FindButtonList()
+    {
+        // If the plugin has broken, this is probably what needs to be updated.
+        return Resources.FindObjectsOfTypeAll<Transform>()
+            .FirstOrDefault(obj => obj.name == "EscapeMenu")?
+            .Find("MainPage/LIST")?.gameObject;
+    }
+}
+
+internal static class LobbyUtils
+{
+    private static CSteamID? CurrentID
+    {
+        get
+        {
+            if (MainMenuHandler.SteamLobbyHandler == null)
+            {
+                return null;
+            }
+
+            return Traverse.Create(MainMenuHandler.SteamLobbyHandler)
+                .Field("m_CurrentLobby")
+                .GetValue<CSteamID>();
+        }
+    }
+
+    private static bool MasterClient
+    {
+        get
+        {
+            return MainMenuHandler.SteamLobbyHandler?.MasterClient ?? false;
+        }
+    }
+
+    internal static bool MakePublic()
+    {
+        CSteamID? id = CurrentID;
+        if (id == null)
+        {
+            Plugin.CurLogger?.LogWarning("Cannot make the lobby public because it does not exist.");
+            return false;
+        }
+
+        if (!MasterClient)
+        {
+            Plugin.CurLogger?.LogWarning("Cannot make the lobby public because you are not the master client.");
+            return false;
+        }
+
+        SteamMatchmaking.SetLobbyType(id.Value, ELobbyType.k_ELobbyTypePublic);
+        SteamMatchmaking.SetLobbyJoinable(id.Value, true);
+
+        // Allows players to join the game after it has started.
+        if (PhotonNetwork.CurrentRoom != null)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = true;
+            PhotonNetwork.CurrentRoom.IsVisible = true;
+        }
+
+        return true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Content Warning: BetterLobby 🌐</h1>
+
+<div align="center">
+  <img src="https://img.shields.io/badge/license-MIT-blue.svg"/>
+</div>
+
+<br>
+
+> BetterLobby is a mod that improves the matchmaking system of _Content Warning_ by adding
+> a __FILL LOBBY__ button in the pause menu
+
+## Features
+
+- Invite your friends in the lobby, and then fill it up with other people
+- Fill the lobby back up when someone quits, even after the game has stated
+- Avoid the standard matchmaking experience, which makes it hard to join a session
+without an error
+
+## Usage
+
+- Install [BepInEx](https://docs.bepinex.dev/articles/user_guide/installation/index.html) in
+the `Content Warning` game directory
+- Copy the `BetterLobby.dll` file in the `BepInEx / plugins` folder
+- Launch the game, click on __PLAY WITH FRIENDS__, and host a game
+- Invite the friends you want to play with, then press <kbd>ESC</kbd> to open the pause menu
+- Click on the new __FILL LOBBY__ button to open the matchmaking to the public
